7ec520c8e63eb3f89a3437b84b9895e1b6a6f698
Vishrut Patel <vrp101@gmail.com>
2012-11-18 04:33:24 +0530

    removing unnecessary files

---
+++ about.php
@@ -0,0 +1,98 @@
+﻿<!DOCTYPE html>
+
+<head>
+
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+
+
+  <title>Welcome to Emo!</title>
+  
+  <!-- Included CSS Files (Uncompressed) -->
+  <!--
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+  -->
+  
+  <!-- Included CSS Files (Compressed) -->
+  <link rel="stylesheet" href="stylesheets/foundation.min.css">
+  <link rel="stylesheet" href="stylesheets/app.css">
+  <link rel="stylesheet" href="stylesheets/presentation.css">
+
+  <script src="javascripts/modernizr.foundation.js"></script>
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+  <div class="container top-bar home-border">
+    <div class="attached">
+      <!--div class="name" onclick="void(0);">
+        <span><a href="http://foundation.zurb.com">Foundation</a> <a href="#" class="toggle-nav"></a></span>
+  		</div-->
+		<ul class="left">
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+		
+        
+  			<li>
+  			  <a href="https://github.com/vishrut/Emo" >Source</a>
+  			</li>
+        </ul>
+  	</div>
+  </div>
+<header>
+  <div class="row">
+    <div class="twelve columns">
+      <h1>Welcome to Emo!</h1>
+      <h4>Emo uses the <strong>Lymbix API</strong> to give a real-time sentiment analysis of your article.</h4>
+    </div>
+  </div>
+ </header> 
+
+  <div class="row">
+    <div class="eight columns">
+
+      <!-- Grid Example -->
+      <div class="row">
+        <div class="twelve columns">          
+		<h4>Ahoy there!</h4>
+		<hr>
+		<h5>Emo is a very small hack created in just 10 hours. It makes REST calls to the <a href="http://www.lymbix.com">Lymbix API </a>to gauge the dominant emotions in your article. And yes, it's been taught to do that in real-time, i.e. as you type along. </h5>
+		<hr>
+		<h5>The front-end has been set up using the framework <a href="http://foundation.zurb.com/">Foundation</a> made by Zurb</h5>
+		<hr><h5>
+		The developer is a poor guy! So he's using the free hosting service provided by <a href="https://www.cloudcontrol.com/">CloudControl</a><hr></h5>
+		<h5>
+		In case you are interested, you can find the source for the application on <a href="https://github.com/vishrut/Emo">GitHub</a>
+		<hr>
+		Thanks for your interest in Emo!</h5>
+		
+        </div>
+      </div>      
+    </div>
+
+  </div>
+  <footer>
+  <div id="copyright">
+    <div class="row">
+      <div class="four columns">
+        <p>Created by Vishrut Patel; 2012</p>
+      </div>
+
+    </div>
+	</div>
+</footer>
+  <!-- Included JS Files (Compressed) -->
+  <script src="javascripts/jquery.js"></script>
+  <script src="javascripts/foundation.min.js"></script>
+  
+  <!-- Initialize JS Plugins -->
+  <script src="javascripts/app.js"></script>
+  
+</body>
+</html>
